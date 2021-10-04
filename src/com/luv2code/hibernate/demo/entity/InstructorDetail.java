8ef726c04d335adcb7b8e075b1e6a9fcfef10e1a
salmanasad123 <slman.me@gmail.com>
2021-10-04 17:42:42 +0500

    Initial commit

---
+++ src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,67 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+
+// annotate the class as entity to map to the database table
+@Entity
+public class InstructorDetail {
+
+    // define the fields and annotate the fields with database column names
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    // no arg constructor
+    public InstructorDetail(){
+
+    }
+
+    // create constructor , we are excluding id as this will be auto incremented
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getter and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // generate toString() method
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
